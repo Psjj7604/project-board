fce5ad9547383d05ac6a0d08c3acf1d37c2358ea
seongjun <psjj7604@gmail.com>
2023-03-15 03:38:42 +0900

    test에서 Auditing 기능을 사용하기 위해 엔티티에 @EntityListeners(AuditingEntityListener.class) 애노테이션 추가

--- src/main/java/com/example/projectboard/domain/Article.java
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -7,10 +7,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -21,6 +24,7 @@ import java.util.Objects;
         @Index(columnList = "createdBy")
 })
 
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Article {
     @Id
@@ -35,6 +39,11 @@ public class Article {
     @Setter
     private String hashtag; //해시태그
 
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleCommentSet = new LinkedHashSet<>();
+
     @CreatedDate
     @Column(nullable = false)
     private LocalDateTime createdAt; //생성일시
@@ -48,7 +57,8 @@ public class Article {
     @Column(nullable = false, length = 100)
     private String modifiedBy; //수정자
 
-    protected Article() {}
+    protected Article() {
+    }
 
     private Article(String title, String content, String hashtag) {
         this.title = title;
@@ -59,7 +69,7 @@ public class Article {
     public static Article of(String title, String content, String hashtag) {
         return new Article(title, content, hashtag);
     }
-    
+
     // 동등성 검사
     @Override
     public boolean equals(Object o) {
